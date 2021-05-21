495b75b3a20688ea6a4ec0ce7a7905cb7c59a4cb
kadiri <abdul_7get@yahoo.com>
2021-05-21 09:30:14 +0100

    initial commit

---
+++ celcius.js
@@ -0,0 +1,77 @@
+// question 1
+
+/*function convertFahrToCelsius(f) {
+     
+    if (Number || String){
+        var fah = (f-32) * 5/9;
+        fah= fah.toFixed(4)
+        console.log(fah)
+    
+    }
+    else{
+
+        console.log(`${fah} + "is not  a valid number but a/an" + ${typeof(fah)} `)
+
+    }
+
+    
+
+} 
+    
+
+convertFahrToCelsius(0) 
+convertFahrToCelsius("0")
+convertFahrToCelsius([1,2,3])
+convertFahrToCelsius({"temp":0})
+*/ 
+// question two
+function checkYuGiOh(num){
+    var num;
+     if (isNaN(num)==true){
+        if (Array.isArray(num)==true){
+            console.log("invalid parameter: " + JSON.stringify(num));
+            return num;
+         } else if (typeof(num)=="object"){
+            console.log("invalid paramter: " + JSON.stringify(num));
+            return num;
+         } else {
+            console.log("invalid paramter: " + JSON.stringify(num));
+            return num;
+         }
+     }
+     else {
+         var ans;
+
+         //assigning the check function to a variable and passing the parameter in the checkYuGiOh function to it
+         ans = create(num);
+
+         //a function that creates an array of numbers from 1 to n and replaces multiples of 2, 3, and 5 with "yu", "gi" and "oh"
+         function create(n){
+            var n;
+            var arr = [];
+            for (i=1; i<=n; i++){
+                if (i%2===0 && i%3===0){
+                   arr.push("yu-gi");
+                }else  if (i%2===0 && i%5===0){
+                   arr.push("yu-oh");
+                } else  if (i%3===0 && i%5===0){
+                   arr.push("gi-oh");
+                } else  if (i%5==0){
+                   arr.push("oh");
+                } else  if (i%3==0){
+                   arr.push("gi");
+                } else  if (i%2==0){
+                   arr.push("yu");
+                } 
+                else{
+                   arr.push(i);
+                }
+              console.log(arr);
+            }
+            return arr;
+           }
+           return ans;
+     }}
+checkYuGiOh(10);
+checkYuGiOh("5");
+checkYuGiOh("fizzbuzz is meh");
